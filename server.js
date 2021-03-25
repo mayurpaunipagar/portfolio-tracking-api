b8a4fb42ac83d557e519152c023ba0370ae638d0
MayurHpNotebook <001mayur94@gmail.com>
2021-03-25 15:32:56 +0530

    error corrected

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 app.get('/',(req,res)=>{
     res.send("Welcome to portfolio tracking api");
-}
+})
 app.listen(process.env.PORT || 9000,()=>{
     console.log("listening @ ", (process.env.PORT || 9000));
 })
