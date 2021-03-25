c1e4bf367692445614e69b1a9820c01464435164
MayurHpNotebook <001mayur94@gmail.com>
2021-03-25 15:27:51 +0530

    updated '/' path

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const app=express();
+app.get('/',(req,res)=>{
+    res.send("Welcome to portfolio tracking api");
+}
 app.listen(process.env.PORT || 9000,()=>{
     console.log("listening @ ", (process.env.PORT || 9000));
 })
